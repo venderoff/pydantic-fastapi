a85353661f1d8171dcc9847379af1d6218a858a1
venderoff <venderoff@gmail.com>
2026-09-06 22:10:42 +0530

    base response added

--- main.py
+++ main.py
@@ -26,30 +26,42 @@ def get_employees():
      )
     # return response
 
-@fastapi_app.post("/employees", response_model=Employee, tags=["Create Employee"])
+# @fastapi_app.post("/employees", response_model=Employee, tags=["Create Employee"])
+@fastapi_app.post("/employees", response_model=base_response, tags=["Create Employee"])
 def create_employee(newEmployee: Employee):
     for e in employees:
         if e.id == newEmployee.id:
             raise HTTPException(status_code=400, detail=f"Employee with id {e.id} already exists")
     employees.append(newEmployee)
-    return newEmployee
 
-@fastapi_app.get("/employees/{id}", response_model=Employee, tags=["getById"])
+    responce = base_response(code=status.HTTP_200_OK, status=str(status.HTTP_200_OK), data=newEmployee, message="successful addition" )
+
+    return JSONResponse(
+        status_code=responce.code,
+        content=responce.model_dump()
+    )
+
+@fastapi_app.get("/employees/{id}", response_model=base_response, tags=["getById"])
 def get_by_id(id:int):
     for e in employees:
         if(e.id == id):
-            return e
-    raise HTTPException(status_code=400, detail=f"Id does not exist{id}")
+            response= base_response(code=status.HTTP_200_OK, data=e, message="success", status=str(status.HTTP_200_OK))
+            return JSONResponse(status_code=response.code, content=response.model_dump())
+    response= base_response(code=status.HTTP_400_BAD_REQUEST,  message=f"Id does not exist{id}", status=str(status.HTTP_400_BAD_REQUEST), data="")    
+    return JSONResponse(status_code=response.code, content=response.model_dump())
 
-@fastapi_app.delete("/employees/{id}", tags=["deleteById"])
+@fastapi_app.delete("/employees/{id}", tags=["deleteById"], response_model=base_response)
 def deleteById(id:int):
     for e in employees:
         if(e.id == id):
             employees.remove(e)
-            return {"status": "ok", "message": f"employee with {id} deleted"}
-    raise HTTPException(status_code=400, detail=f"employee not found {id}")    
+            response=base_response(status=str(status.HTTP_200_OK), data=e, message="deleted successfully", code=status.HTTP_200_OK)
+            return JSONResponse(content=response.model_dump(), status_code=response.code)   
+            # return {"status": "ok", "message": f"employee with {id} deleted"}
+    response=base_response(status=str(status.HTTP_400_BAD_REQUEST), data="", message=f"id= {id} not available", code=status.HTTP_400_BAD_REQUEST)        
+    return JSONResponse(content=response.model_dump(), status_code=response.code)    
 
-@fastapi_app.put("/employees/{id}", response_model=Employee, tags=["update"])
+@fastapi_app.put("/employees/{id}", response_model=base_response, tags=["update"])
 def updateEmployee(id:int, updatedEmployee: EmployeeUpdate):
     for e in employees:
         if(e.id == id):
@@ -59,7 +71,8 @@ def updateEmployee(id:int, updatedEmployee: EmployeeUpdate):
                 e.name = updatedEmployee.name
             if updatedEmployee.salary is not None:
                 e.salary = updatedEmployee.salary
-            return e 
-    raise HTTPException(status_code=400, detail=f"Data not dound for {id}")            
-
+            response=base_response(status=str(status.HTTP_200_OK), data=e, message="updated successfully", code=status.HTTP_200_OK)
+            return JSONResponse(content=response.model_dump(), status_code=response.code) 
+    response=base_response(status=str(status.HTTP_400_BAD_REQUEST), data="", message="id = {id} not found", code=status.HTTP_400_BAD_REQUEST)
+    return JSONResponse(content=response.model_dump(), status_code=response.code)
     
